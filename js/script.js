d7939733e23d87a2f4cf2c6a41747d620d99b289
bmitts3 <mittsb.24@william.jewell.edu>
2025-11-01 18:46:00 -0500

    debugged so that it clears the gifs from previous clicks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 console.log("script.js loaded");
 endpoint="https://api.giphy.com/v1/gifs/search?api_key=QC0ZmALB1039VA0CSlDY8QDkUA5IBxA6&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
 images=[];
+//asynchromous function to fetch gifs from giphy api
 async function FetchGif() {
     const response = await fetch(endpoint);
     const data = await response.json();
@@ -10,7 +11,7 @@ async function FetchGif() {
     const output = document.getElementById("gif-container"); 
     output.textContent = gif;
 };
-
+//event listener for button to fetch gifs on click
 let GifContainer=document.querySelector("#gif-container");
 let button=document.querySelector("#fetch-gif-btn");
 button.addEventListener("click", async function(){
@@ -18,11 +19,10 @@ button.addEventListener("click", async function(){
     let data=await response.json();
     console.log(data);
     let gif=data.data[0];
+    GifContainer.innerHTML = ""; 
     const images = data.data.map(gif => gif.images.original.url);
     for (const imageUrl of images) {
-        GifContainer.innerHTML += `
-        <img src="${imageUrl}" alt="dog gif" class="col-3 mb-3">
-        `;
+        GifContainer.innerHTML += `<img src="${imageUrl}" alt="dog gif" class="col-3 mb-3">`;
     }
 
 });
